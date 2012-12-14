4bf4eca913df373aebe5db1c5ae3aba7cf8baab8
minamo <ph.minamo@cytisan.com>
2012-12-14 10:31:03 +0800

    Export platform function directly on navigate-url.

--- sabrina/navigate-url.js
+++ sabrina/navigate-url.js
@@ -2,20 +2,20 @@
 
 var exec = require('child_process').exec;
 
-function darwin (url) {
-    exec('open "' + url + '"');
-}
-
-function win32 (url) {
-    // http://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/ntcmds_shelloverview.mspx
-    exec('start ' + url.replace(/(\(|\)|\||&)/g, "^$&"));
-}
-
-module.exports = function (url) {
-    switch (process.platform) {
-        case "darwin":
-            return darwin(url);
-        case "win32":
-            return win32(url);
+var implementations = {
+    darwin: function darwin (url) {
+        exec('open "' + url + '"');
+    }
+    , win32: function win32 (url) {
+        // http://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/ntcmds_shelloverview.mspx
+        exec('start ' + url.replace(/(\(|\)|\||&)/g, "^$&"));
     }
 };
+
+module.exports = (function (platform) {
+    var func = implementations[process.platform];
+    if (func) {
+        return func;
+    }
+    return (function noop () {});
+})(process.platform);
